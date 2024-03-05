66c01a2a31bb027840f1186aa6e31c5ed91836b4
jacobo <juanjacobojaramillo@gmail.com>
2024-03-05 15:26:39 -0500

    Initial commit

--- app/src/main/java/co/edu/unipiloto/stopwatch2/MainActivity.java
+++ app/src/main/java/co/edu/unipiloto/stopwatch2/MainActivity.java
@@ -22,12 +22,12 @@ public class MainActivity extends AppCompatActivity {
     private String[] guardados = new String[5];
     private int guardarNo = 0;
 
+    String resultado = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         // Button button=findViewById(R.id.boton_iniciar);
         // button.setOnClickListener(new View.onClickListener());
         runTimer();
@@ -38,6 +38,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onClickStart(View view){
+
         running=true;
     }
     public void onClickPause(View view){
@@ -46,17 +47,24 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onClickReset(View view){
+        TextView timeView2 = (TextView) findViewById(R.id.Vueltas);
         running=false;
         segundos=0;
+        resultado = "";
+        timeView2.setText("");
+        for(int i = 0; i<5;i++){
+            guardados[i]="";
+        }
+        guardarNo = 0;
     }
 
     public void onClickSave(View view){
-        TextView timeView2 = (TextView)findViewById(R.id.Vueltas);
+        TextView timeView2 = (TextView) findViewById(R.id.Vueltas);
         int hora = segundos/3600;
         int minutes = (segundos %3600)/60;
         int secs = segundos %60;
         String tiempo2 = String.format(Locale.getDefault(),"%d:%02d:%02d",hora,minutes,secs);
-        String resultado = "";
+        resultado = "";
         guardados[guardarNo] = tiempo2;
         for(int i = 0; i<5;i++){
             resultado += guardados[i]+"\n";
@@ -70,27 +78,25 @@ public class MainActivity extends AppCompatActivity {
 
     private void runTimer(){
         //relacionar un objeto textview con el elemento grafico correspondiente
-        TextView timeView = (TextView) findViewById(R.id.time_view);
+        TextView timeView = (TextView) findViewById(R.id.tiempoVivo);
 
         //declarar un handler para manejar un hilo
         Handler handler = new Handler();
         //invocar el metodo post e instanciar un objeto runneable
-        timeView.post(new Runnable() {
+        handler.post(new Runnable() {
             @Override
             public void run() {
-                int horas=segundos/3600;
-                int minutos = (segundos%360)/60;
-                int secs = segundos%60;
+                int hora = segundos/3600;
+                int minutes = (segundos %3600)/60;
+                int secs = segundos %60;
 
-                //formato de salida
-                String tiempo = String.format(Locale.getDefault(),"%d:%02d:%02d", horas,minutos,secs);
+                String tiempo = String.format(Locale.getDefault(),"%d:%02d:%02d",hora,minutes,secs);
 
                 timeView.setText(tiempo);
-                if(running){
-                    segundos++;
-                    timeView.postDelayed(this, 1000);
-                }
 
+                if (running)
+                    segundos++;
+                handler.postDelayed(this,1000);
 
             }
         });
